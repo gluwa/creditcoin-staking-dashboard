polkadot-cloud/polkadot-staking-dashboard

re-added staking pools

src/library/Graphs/PayoutLine.tsx
@@ -13,8 +13,11 @@ import {
   Tooltip,
 } from 'chart.js';
 import { useApi } from 'contexts/Api';
+import { usePoolMemberships } from 'contexts/Pools/PoolMemberships';
+import { useStaking } from 'contexts/Staking';
 import { useSubscan } from 'contexts/Subscan';
 import { useTheme } from 'contexts/Themes';
+import { useUi } from 'contexts/UI';
 import { Line } from 'react-chartjs-2';
 import { useTranslation } from 'react-i18next';
 import { graphColors } from 'styles/graphs';
@@ -44,15 +47,14 @@ export const PayoutLine = ({
 }: PayoutLineProps) => {
   const { t } = useTranslation('library');
   const { mode } = useTheme();
-  const { unit, units } = useApi().network;
   const { payouts, poolClaims } = useSubscan();
+  const { unit, units, colors } = useApi().network;
+  const { isSyncing } = useUi();
+  const { inSetup } = useStaking();
+  const { membership: poolMembership } = usePoolMemberships();
 
-  // Unneeded until Pools are officially supported
-  // const { isSyncing } = useUi();
-  // const { inSetup } = useStaking();
-  // const { membership: poolMembership } = usePoolMemberships();
-  // const notStaking = !isSyncing && inSetup() && !poolMembership;
-  // const poolingOnly = !isSyncing && inSetup() && poolMembership !== null;
+  const notStaking = !isSyncing && inSetup() && !poolMembership;
+  const poolingOnly = !isSyncing && inSetup() && poolMembership !== null;
 
   // remove slashes from payouts (graph does not support negative values).
   const payoutsNoSlash = payouts.filter(
@@ -86,7 +88,11 @@ export const PayoutLine = ({
   );
 
   // determine color for payouts
-  const color = '#9cffaa';
+  const color = notStaking
+    ? colors.primary[mode]
+    : !poolingOnly
+    ? colors.primary[mode]
+    : colors.secondary[mode];
 
   // configure graph options
   const options = {

gluwa/creditcoin-staking-dashboard

src/library/Graphs/PayoutLine.tsx
@@ -48,15 +48,13 @@ export const PayoutLine = ({
   const { t } = useTranslation('library');
   const { mode } = useTheme();
   const { payouts, poolClaims } = useSubscan();
-  const { unit, units } = useNetwork().networkData;
+  const { unit, units, colors } = useNetwork().networkData;
+  const { isSyncing } = useUi();
+  const { inSetup } = useStaking();
   const { membership: poolMembership } = usePoolMemberships();
 
-  // Unneeded until Pools are officially supported
-  // const { isSyncing } = useUi();
-  // const { inSetup } = useStaking();
-  // const { membership: poolMembership } = usePoolMemberships();
-  // const notStaking = !isSyncing && inSetup() && !poolMembership;
-  // const poolingOnly = !isSyncing && inSetup() && poolMembership !== null;
+  const notStaking = !isSyncing && inSetup() && !poolMembership;
+  const poolingOnly = !isSyncing && inSetup() && poolMembership !== null;
 
   // remove slashes from payouts (graph does not support negative values).
   const payoutsNoSlash = payouts.filter(
@@ -90,7 +88,11 @@ export const PayoutLine = ({
   );
 
   // determine color for payouts
-  const color = '#9cffaa';
+  const color = notStaking
+    ? colors.primary[mode]
+    : !poolingOnly
+    ? colors.primary[mode]
+    : colors.secondary[mode];
 
   // configure graph options
   const options = {