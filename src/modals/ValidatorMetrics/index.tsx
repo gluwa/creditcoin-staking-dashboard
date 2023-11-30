c986ac43a29bd1933a9c7871a008f4233a9fb3fa
hyeonggook.lee <hyeonggook.lee@gluwa.com>
2023-11-30 11:19:16 +0900

    Fix subscan link error:

--- src/modals/ValidatorMetrics/index.tsx
+++ src/modals/ValidatorMetrics/index.tsx
@@ -108,7 +108,9 @@ export const ValidatorMetrics = () => {
         className="body"
         style={{ position: 'relative', marginTop: '0.5rem' }}
       >
-        <SubscanButton validatorAddress={validatorInEra.address} />
+        {validatorInEra?.address && (
+          <SubscanButton validatorAddress={validatorInEra?.address} />
+        )}
         <GraphWrapper
           style={{
             margin: '0 1.5rem 0 0.5rem',
